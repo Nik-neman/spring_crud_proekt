9670a0ebfd4ec4f5319e7dda79f24054334d8a12
Nikolai <nik-neman@mail.ru>
2022-07-22 16:47:03 +0200

    Initial commit

---
+++ src/main/java/com/example/spring_crud_proekt/SpringCrudProektApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_crud_proekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudProektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudProektApplication.class, args);
+    }
+
+}
